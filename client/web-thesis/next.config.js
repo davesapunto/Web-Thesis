131f00b6f235aa40662849d3ea5e8b0db3d72f8b
John Dave Sapunto <118431829+davesapunto@users.noreply.github.com>
2024-11-22 22:14:14 +0800

    Update next.config.js

--- client/web-thesis/next.config.js
+++ client/web-thesis/next.config.js
@@ -1,10 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/Web-Thesis', // Replace with your repository name
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
-
-export default nextConfig
+ 
+module.exports = nextConfig
