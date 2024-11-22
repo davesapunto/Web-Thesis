3982b3315c55cbd45436eebc0540f325447b98e4
John Dave Sapunto <118431829+davesapunto@users.noreply.github.com>
2024-11-22 20:24:52 +0800

    yes

--- client/web-thesis/next.config.mjs
+++ client/web-thesis/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
+    output: 'export',  // Required for static site generation
+    images: {
+      unoptimized: true, // Required for static export
+    },
+    basePath: '/Web-Thesis', // Replace with your repository name
   };
   
-  module.exports = nextConfig
+  module.exports = nextConfig;
